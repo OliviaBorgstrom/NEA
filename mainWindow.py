637edf306112023a46ecfbe22e22365021e1307a
Liviturtle <livielovie@outlook.com>
2020-12-08 00:28:57 +0000

    Update mainWindow.py
    testing to make tabs

--- mainWindow.py
+++ mainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QTabWidget
+from PyQt5.QtWidgets import QApplication, QDialog, QTabWidget, QWidget
 from PyQt5.QtGui import QIcon
 import sys
 
@@ -9,6 +9,37 @@ class TabWidget(QDialog):
 
         self.setWindowTitle("NEL Wastebase")
         self.setWindowIcon(QIcon("gearicon.jpg"))
+
+        #tabs = [[homeTab(),"home"],[createTab(),'create'],[importTab(),'import'],[viewTab(),'view']]
+        #for i in range(len(tabs)):
+        tabmenu = QTabWidget
+        tabmenu.addTab(homeTab(),'home')
+        tabmenu.addTab(createTab(),'create')
+        tabmenu.addTab(importTab(),'import')
+        tabmenu.addTab(viewTab(),'view')
+        #tabmenu is a PyQt5 widget which allows for tabs to be set out
+        
+        mainbox = QVBoxlayout().addWidget(tabmenu) 
+        self.setLayout(mainbox)
+        #mainbox is the encompassing layout for the whole window, the tabs are added to a box.
+
+
+class homeTab(QTabWidget):
+    def __init__(self):
+        super().__init__()
+
+class createTab(QTabWidget):
+    def __init__(self):
+        super().__init__()
+
+class importTab(QTabWidget):
+    def __init__(self):
+        super().__init__()
+
+class viewTab(QTabWidget):
+    def __init__(self):
+        super().__init__()
+
 
 app = QApplication(sys.argv)
 mainWindow = TabWidget()
