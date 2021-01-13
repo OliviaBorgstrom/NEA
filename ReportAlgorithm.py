2e20e1b9345a92cede2f667bbbd480382c65fe0c
Liviturtle <livielovie@outlook.com>
2021-01-13 15:42:17 +0000

    updated

--- ReportAlgorithm.py
+++ ReportAlgorithm.py
@@ -1,9 +1,17 @@
 # do some analysing!
-import numpy
+# import numpy
 # from Database import ... # will need to make some new functions for this?
 import matplotlib
 
 class graphobj(object):
     def __init__(self,data,title):
         self.data = data
-        self.title = title
+        self.title = plt.title(title)
+
+        x = [1,2,3]
+        y = [2,4,1]
+        self.xlabel = plt.xlabel('x - axis')
+        self.ylabel = plt.ylabel('y - axis')
+
+graph1 = graphobj('insert data','a test graph')
+plt.show()
