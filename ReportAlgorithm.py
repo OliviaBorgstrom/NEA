3c739564728568b4a88bf88223fe3e7fbcda0d7a
Liviturtle <livielovie@outlook.com>
2021-01-13 12:27:30 +0000

    created ReportAlgorithm.py

--- ReportAlgorithm.py
+++ ReportAlgorithm.py
@@ -1,3 +1,9 @@
-#do some analysing!
+# do some analysing!
 import numpy
 # from Database import ... # will need to make some new functions for this?
+import matplotlib
+
+class graphobj(object):
+    def __init__(self,data,title):
+        self.data = data
+        self.title = title
