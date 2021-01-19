01326c285098e68e1fc3796a71bf73c84fd27c70
Liviturtle <livielovie@outlook.com>
2021-01-19 13:34:30 +0000

    Program now generates graph pngs for each site, next need to try it for real data rather than sample

--- Create_Report.py
+++ Create_Report.py
@@ -8,6 +8,7 @@ class array_circ(object):
     def __init__(self,data,pointer):
         self.data = data
         self.pointer = pointer
+        self.slicelst_indexes = []
     
     def __str__(self):
         return self.data
@@ -27,20 +28,28 @@ class array_circ(object):
         else:
             self.pointer -= 1
 
-    def getto(self,index):  # index the amount away from current position
+    def get_to(self,index):  # index the amount away from current position
         slicelst = []
+        slicelst_indexes = []
         slicelst.append(self.data[self.pointer])  # i want it to include the current month
+        slicelst_indexes.append(self.pointer + 1)
         if index > 0:
             for i in range(abs(index)):
                 self.__movepointerup()
                 slicelst.append(self.data[self.pointer])
+                slicelst_indexes.append(self.pointer + 1)
         elif index < 0:
             for i in range(abs(index)):
                 self.__movepointerdown()
                 slicelst.append(self.data[self.pointer])
+                slicelst_indexes.append(self.pointer + 1)
         else:
             return slicelst
+        self.currentsliceindexes = slicelst_indexes
         return slicelst
+    
+    def get_to_asint(self):
+        return self.currentsliceindexes
     
     # will make another function to get between two values, or set pointer to somewhere then getto
             
@@ -49,14 +58,13 @@ class siteAnalysis(object):  # right now this only works with reports starting f
         self.entries = entries
         self.reporttype = reporttype
         self.datefrom = datefrom
+        self.dateto = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0)
         self.months = array_circ(['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],(datetime.today().month - 1))
         self.sitename = siteinfo[1]
         self.amtplasticbin = siteinfo[2]
         self.amtpaperbin = siteinfo[3]
         self.amtglassbin = siteinfo[4]
-        
 
-       
         #self.latestplastic = latest[0]  # these were for potentially showing the start --> end values
         #self.latestpaper = latest[1]
         #self.latestglass = latest[2]
@@ -65,20 +73,12 @@ class siteAnalysis(object):  # right now this only works with reports starting f
         #self.oldestpaper = oldest[1]
         #self.oldestglass = oldest[2]
     
-        self.initTimes()
-        self.generategraphpng(entries,'dummypath')
         self.generatexvalues()
+        self.generategraphpng(entries,'dummypath')
 
         # maybe have an overall site usage - e.g calculate the mean usage over time
         # and mean usage total over the time period
         # bubble chart at end of report showing most and least used sites 'means'
-    
-    def initTimes(self):
-        self.thismonth = datetime.today().replace(day=1,hour=0, minute=0, second=0, microsecond=0)
-        self.thisyear = datetime.today().replace(day=1,month=1,hour=0, minute=0, second=0, microsecond=0)
-        self.sevendaysago = (datetime.today() - timedelta(days=7)).replace(hour=0, minute=0, second=0, microsecond=0)
-        self.past12months = (datetime.today() - timedelta(days=365)).replace(day=1,hour=0, minute=0, second=0, microsecond=0)
-        self.past30days = (datetime.today() - timedelta(days=30)).replace(hour=0, minute=0, second=0, microsecond=0)
 
     def gettitle(self):
         return self.sitename
@@ -87,46 +87,55 @@ class siteAnalysis(object):  # right now this only works with reports starting f
         self.usage_chart = pygal.DateLine(x_label_rotation=25)
         self.usage_chart.title = 'Usage over time for ' + self.sitename
         self.usage_chart.y_labels = list(i for i in range(0, 110,10))  # 10 is the step
+        self.usage_chart.x_labels = self.x_values
         self.usage_chart.show_x_guides = True  # show grid lines for both axis
         self.usage_chart.show_y_guides = True
+        self.usage_chart.__init__
+        self.usage_chart.add("Plastic", [
+            (date(2020, 1, 2), 30),
+            (date(2020, 8, 15), 75),
+            (date(2020, 12, 7), 50),
+            (date(2020, 3, 21), 100)])
+        
+        self.usage_chart.render_to_png((self.sitename).replace(' ','_') + '.png')
     
-    def months_since(self,target):  # can make this months between eventually
-        today = datetime.today().replace(hour=0, minute=0, second=0, microsecond=0) 
-        num_months = (today.year - target.year) * 12 + (today.month - target.month)
+    def months_since(self,target):  # can make this months between when needed
+        num_months = (self.dateto.year - target.year) * 12 + (self.dateto.month - target.month)
         return num_months
 
-    def generatexvalues(self):
+    def generatexvalues(self):  # maybe this should be done outside the function if they are all for same time
+        # because currently it is repeated for each site which is unecessary
+        # maybe put it in an init of a report class when it is made
         months_since = self.months_since(self.datefrom)
+        self.x_values = ()  # a tuple
+        labels = []
+        years = []
+
         if months_since == 0:
             self.checkweeklymonthly()
         else:
-            monthlist = self.months.getto((0 - months_since))
-            print(monthlist)
+            monthlist = self.months.get_to((0 - months_since))
+            int_months = self.months.get_to_asint()
+            
+            year = self.dateto.year
+            for i in range(len(monthlist)):  # adds the correct year to each month
+                labels.append(monthlist[i] + ' ' + str(year))
+                years.append(year)
+                if i != len(monthlist) - 1:
+                    if monthlist[i] == 'Jan' and monthlist[i + 1] == 'Dec':
+                        year -= 1
+            
+            values = list(map(lambda x,y: date(x,y,1),years,int_months))
+
+            for i in range(len(labels)):
+                tempdict = {}
+                tempdict['label'] = labels[i]
+                tempdict['value'] = values[i]
+                self.x_values = self.x_values + (tempdict, )
 
     def checkweeklymonthly(self):
         pass
         
-    def presetxvalues(self,reporttype):
-        switcher = {
-            'This Year':self.thisyear_x(),
-            'Past 7 Days':self.sevendays_x(),
-            'This Month': self.thismonth_x(),
-            'This Quarter': self.thisquarter_x()
-        }
-        return switcher.get(reporttype,'Error: unknown time frame')
-    
-    def thisyear_x(self):
-        pass
-    
-    def thismonth_x(self):
-        pass
-
-    def sevendays_x(self):
-        pass
-
-    def thisquarter_x(self):
-        pass
-
     def generatemeandata(self,label,data):
         pass  # find means of all the data entries
 
@@ -163,7 +172,7 @@ def sublists(lookfor,lst,namesonly):  # sitedata has been alphabetically ordered
     
 #when using this on another computer, need to find the path of the current file name to save there
 #callAnalysis(datetime.today().replace(day=1,month=1,hour=0, minute=0, second=0, microsecond=0),['Asda Ellis Way','Beeston Street','Pier','Boating lake'])
-callAnalysis(date(2020,5,30),['Asda Ellis Way','Beeston Street','Pier','Boating lake'])  # maybe i can make an advanced customisability at some point
+callAnalysis(date(2019,1,1),['Asda Ellis Way','Beeston Street','Pier','Boating lake'])  # maybe i can make an advanced customisability at some point
 # slightly problematically, pygal uses utc time by default -- might not cause any issues still
 
 # have a 'this month feature', 'past 30 days feature'
