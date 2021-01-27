103c686c12d7807f52d35ea91a3b61f82fa9e434
Liviturtle <livielovie@outlook.com>
2021-01-27 11:09:49 +0000

    created a template to reorganise the report class

---
+++ Reportclass_structure.py
@@ -0,0 +1,23 @@
+class Report(object):
+    def __init__(self):
+        self.dataObjects = []
+    def initAnalysisObj(self):
+        pass
+    
+    def generatexvalues():
+        pass
+    
+    def renderhtml(self):
+        pass
+
+class Analysis(object):  # those which arent specifically monthly or weekly are just Analysis
+    def __init__(self):
+        pass
+
+class weeklyAnalysis(Analysis):
+    def __init__(self):
+        pass
+
+class monthlyAnalysis(Analysis):
+    def __init__(self):
+        pass
