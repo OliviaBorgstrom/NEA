ed37ea13b67a2e04a3eb6bfa48c9035b65b6c397
Liviturtle <livielovie@outlook.com>
2021-02-24 00:23:52 +0000

    made sure when editing you, if there are zero of a bin you cant add any data for that bin type

--- QDialog_Edit.py
+++ QDialog_Edit.py
@@ -13,14 +13,21 @@ class EditDialog(QDialog):
         self.setWindowTitle("Editing an entry...")
         self.setFixedSize(600,100)
         self.rowdata = rowdata
+        
         self.user = user
         self.password = password
-        self.host = host 
+        self.host = host
+        
         if locations[0] == 'All':
             locations.pop(0)
+        
         self.locations = locations
         self.entryid = entryid
         self.siteids = siteids
+        
+        self.locationInfo = fetchLocations(user,password,host)
+        print(self.locationInfo)
+        
         self.initeditingBoxes()
       
         buttons = QDialogButtonBox.Save | QDialogButtonBox.Cancel  # change this so that it doesnt trigger when pressing enter
@@ -41,21 +48,34 @@ class EditDialog(QDialog):
         self.siteDropDown = QComboBox()
         self.siteDropDown.addItems(self.locations)
         self.siteDropDown.setCurrentIndex(self.locations.index(self.rowdata[1]))  # set it to be current site id index
+        self.siteDropDown.currentIndexChanged.connect(self.checkIfDisable)
 
         self.glassEdit = QSpinBox()
         self.paperEdit = QSpinBox()
         self.plasticEdit = QSpinBox()
-        boxes = [self.glassEdit,self.paperEdit,self.plasticEdit]
+        self.boxes = [self.glassEdit,self.paperEdit,self.plasticEdit]
 
         self.editGroup = QHBoxLayout()
         self.editGroup.addWidget(self.dateEdit)
         self.editGroup.addWidget(self.siteDropDown)
        
-        for i in range(len(boxes)):
-            boxes[i].setValue(int(self.rowdata[i + 2]))
-            boxes[i].setMaximum(100)  # cant have more than 100%
-            self.editGroup.addWidget(boxes[i])
+        for i in range(len(self.boxes)):
+            self.boxes[i].setValue(int(self.rowdata[i + 2]))
+            self.boxes[i].setMaximum(100)  # cant have more than 100%
+            self.editGroup.addWidget(self.boxes[i])
+        
+        self.checkIfDisable()
     
+    def checkIfDisable(self):
+        x = self.siteDropDown.currentIndex()
+        binnumbers = self.locationInfo[x][2:]
+        print(binnumbers)
+        for i in range(len(binnumbers)):
+            if binnumbers[i] == 0:
+                self.boxes[i].setDisabled(True)
+            else:
+                self.boxes[i].setDisabled(False)
+
     def oncancel(self):
         self.reject()
    
@@ -64,7 +84,15 @@ class EditDialog(QDialog):
         dateboxvalue = self.dateEdit.date()
         #print(dateboxvalue)
         self.date = dateboxvalue.toPyDate()
-        editExisting(self.user,self.password, self.host,self.date,self.selectedlocation,self.glassEdit.value(), self.paperEdit.value(), self.plasticEdit.value(), self.entryid)
+        
+        values = []
+        for i in range(len(self.boxes)):  # what this does is makes sure that if you have changed to a different site with 0 bins in a place, it has a val of 0 at that place
+            if not self.boxes[i].isEnabled():
+                values.append(0)
+            else:
+                values.append(self.boxes[i].value())
+
+        editExisting(self.user,self.password, self.host,self.date,self.selectedlocation,values[0], values[1], values[2], self.entryid)
         self.accept()
             
 #fix the resizing so that a person cant resize the editing window, but it still autoexpands to the right size
