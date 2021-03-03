42301c860b8ab48f19edb410fab6ecde3791c61a
Liviturtle <livielovie@outlook.com>
2021-03-03 12:09:07 +0000

    file are now written when a site has entries- to archive those entries

--- are_you_sure_about_that.py
+++ are_you_sure_about_that.py
@@ -3,24 +3,26 @@ from PyQt5 import QtCore
 from PyQt5 import QtGui
 
 class AreYouSure(QDialog):
-    def __init__(self):
+    def __init__(self,anyEntries,amountOfEntries):
         super(AreYouSure,self).__init__()
-        self.setFixedSize(200,150)
+        self.setFixedSize(300,150)
         self.setWindowTitle("Confirm")
-        important_text = QLabel("Are you sure you want to delete this location?")
+        if anyEntries:
+            self.archive = True  # will still not archive if No is selected
+            text = '''The location you are about to delete has ''' + str(amountOfEntries) + ''' entries attributed to it.\nIf you select 'Yes' to delete, these will be archived.\nAre you sure you want to delete this location?'''
+            important_text = QLabel(text)
+        else:
+            self.archive = False
+            important_text = QLabel("Are you sure you want to delete this location?")
+            
         important_text.setWordWrap(True)
         important_text.setAlignment(QtCore.Qt.AlignCenter)
         important_text.setStyleSheet("font: bold 10pt AGENTORANGE")
         choice = QDialogButtonBox.Yes | QDialogButtonBox.No
-
         ballotbox = QDialogButtonBox(choice)
         ballotbox.accepted.connect(self.accept)
         ballotbox.rejected.connect(self.reject)
         ballotbox.move(0,0)
-
-        #buttonalign = QVBoxLayout()
-        #buttonalign.addWidget(ballotbox)
-        #buttonalign.setAlignment(QtCore.Qt.AlignCenter)
 
         important_layout = QVBoxLayout()
         important_layout.addWidget(important_text)
